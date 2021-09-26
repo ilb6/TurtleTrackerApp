de3926a156e7a5e2c1947357fdfd0ac1d7e1b6b4
ilb6 <89398663+ilb6@users.noreply.github.com>
2021-09-26 17:58:04 -0400

    added dictionaries for date and coordinates

--- ArgosTrackingTool.py
+++ ArgosTrackingTool.py
@@ -8,7 +8,7 @@
 # Author: Isabel Bukovnik (ilb6@duke.edu)
 # Date:   Fall 2021
 #--------------------------------------------------------------
-#Create a variable pointing to the data file
+#Create a variable point to the data file
 file_name = './data/raw/sara.txt'
 
 #Create a file object from the file
@@ -20,19 +20,24 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
-#Pretend we read one line of data from the file
+#Create two empty dictionary objects
+date_dict = {}
+coord_dict = {}
+
+#Iterate through all lines in linelist
 for lineString in line_list:
-    if lineString[0] == "#" or lineString[0] == "u": continue
+    if lineString[0] in ("#",'u'): continue
 
     #Split the string into a list of data items
     lineData = lineString.split()
     
-    #Extract items in list into variables
-    record_id = lineData[0]
-    obs_date = lineData[2]
-    obs_lc = lineData[4]
-    obs_lat = lineData[6]
-    obs_lon = lineData[7]
+    #Extract the items in list into variables
+    record_id = lineData[0]  # ARGOS tracking record ID
+    obs_date = lineData[2]   # Observation date
+    ob_lc = lineData[4]      # Observation Location Class
+    obs_lat = lineData[6]    # Observation Latitude
+    obs_lon = lineData[7]    # Observation Longitude
     
-    #Print the location of sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    date_dict[record_id] = obs_date
+    coord_dict[record_id] = (obs_lat,obs_lon)
